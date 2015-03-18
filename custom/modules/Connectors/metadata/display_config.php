d51a323382ac8004ede0bb281bf1af50f72ff475
unknown <Santosh@Santosh-PC.(none)>
2015-03-18 07:10:30 +0530

    initial commit

---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2015-03-17 19:22:31
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
